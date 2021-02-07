14d36337c0a0862b62c30cd9d46c6cd3a650b088
MarcusGH <29378769+marcusGH@users.noreply.github.com>
2021-02-07 16:44:29 +0100

    Update docs, Change outputs
    The outputs of the process optimiser module was updated
    slightly.
    
    The documentation for the process optimiser was updated.

---
+++ src/cmake-build-debug/CMakeFiles/src.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/src.dir/process_optimiser.cc.o"
+  "src"
+  "src.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/src.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
