9572f3d1f828321f9dd1e8b9d7a5879a301084c8
MarcusGH <29378769+marcusGH@users.noreply.github.com>
2021-02-07 17:11:11 +0100

    Fixed bug in process_optimiser C++ and MATLAB code
    There were some issues with the conflicting types between
    the MATLAB and C++ code.

--- src/process_optimiser.cc
+++ src/process_optimiser.cc
@@ -1,14 +1,15 @@
 #include "process_optimiser.h"
 
-bool process_optimiser_main(const double * box_position,
-        const double * box_dimensions, const int tetromino_id,
-        const int tetrominos, double * tetromino_position, double * tetromino_rotation) {
+bool process_optimiser_main(const double * const box_position,
+        const double * const box_dimensions, const int tetromino_id,
+        const int tetrominos, double * tetromino_position,
+        double * tetromino_rotation) {
 
     tetromino_position[0] = 10;
     tetromino_position[1] = 20;
     tetromino_position[2] = 30;
 
-    tetromino_rotation[0] = 90;
+    tetromino_rotation[0] = 1;
 
     return true;
 }
--- src/process_optimiser.h
+++ src/process_optimiser.h
@@ -1,8 +1,10 @@
 #ifndef PROCESS_OPTIMISER_HEADER
 #define PROCESS_OPTIMISER_HEADER
 
-bool process_optimiser_main(const double * box_position,
-        const double * box_dimensions, const int tetromino_id,
-        const int tetrominos, double * tetromino_position, double * tetromino_rotation);
+bool process_optimiser_main(const double * const box_position,
+        const double * const box_dimensions, const int tetromino_id,
+        const int tetrominos, double * tetromino_position,
+        double * tetromino_rotation);
+
 
 #endif
