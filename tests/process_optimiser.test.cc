9a5b2e866e4e4a263d2ede16a35fca2ce5f113e3
Joe O'Connor <joeoc2625@gmail.com>
2021-02-25 01:03:59 +0000

    Add include on src dir from tests

--- tests/process_optimiser.test.cc
+++ tests/process_optimiser.test.cc
@@ -1,28 +1,7 @@
 #include <catch.hpp>
+#define GET_FULL_PROCESS_OPTIMISER_HEADER
 #include "process_optimiser.h"
 
-SCENARIO("strings can be left-padded") {
-  GIVEN("a string") {
-    using namespace std::string_literals;
-    auto string = "schneide"s;
-
-    WHEN("the string is padded with lower length") {
-      auto length = 4;
-      REQUIRE(length < string.length());
-      THEN("it stays the same") {
-        REQUIRE(string_utils::left_pad(string, length) == string);
-      }
-    }
-
-    WHEN("the string is padded with a higher length") {
-      std::size_t length = 16;
-      REQUIRE(length > string.length());
-      THEN("empty characters are inserted to the left and the length is changed") {
-        auto padded = string_utils::left_pad(string, length);
-        REQUIRE(length == padded.length());
-        auto padding = padded.substr(0, length - string.length());
-        REQUIRE(padding == std::string(8, ' '));
-      }
-    }
-  }
+TEST_CASE( "1 is 1" ) {
+    REQUIRE( 1 == 1 );
 }
